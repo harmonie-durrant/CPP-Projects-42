8b8526cc18a075344e0ef1dfd875961d91a6a39d
harmonie-durrant <harmoniedurrant@gmail.com>
2024-08-31 13:08:29 +0200

    Add CPP-00/ex00

---
+++ CPP-00/ex00/megaphone.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+int main(int ac, char const **av)
+{
+	int	i;
+	int	j;
+
+	i = -1;
+	if (ac < 2)
+	{
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
+		return (0);
+	}
+	while (ac > 1 && av && av[++j])
+	{
+		i = -1;
+		while (j > 0 && av[j][++i])
+			std::cout << (char)std::toupper(av[j][i]);
+	}
+	std::cout << std::endl;
+	return (0);
+}
